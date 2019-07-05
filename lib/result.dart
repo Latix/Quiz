ccb02ee5b293c268df12a338639c33607508c5a7
Kamsi Kodi <kamsikodi@gmail.com>
2019-07-05 12:31:57 +0100

    Added reset handler

--- lib/result.dart
+++ lib/result.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
-  Result(this.resultScore);
+  final Function resetHandler;
+  Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase
   {
@@ -11,11 +12,16 @@ class Result extends StatelessWidget {
   }
   @override
   Widget build(BuildContext context) {
-    return Center(child: 
-      Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-        textAlign: TextAlign.center,
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(child: Text('Restart Quiz'), onPressed: resetHandler),
+        ],
       ),
     );
   }
