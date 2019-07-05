1dd6bc958c96b53e66934820cf8ce20ce88bac9d
Kamsi Kodi <kamsikodi@gmail.com>
2019-07-05 15:26:39 +0100

    Setup tmp

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,9 @@
 import 'package:flutter/material.dart';
-import './quiz.dart';
-import './result.dart';
-// import  './question.dart';
-// import './answer.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  static const _questions = [
-        {
-          'questionsText': 'What is your favorite colour', 
-          'answers':[
-            {'text': 'black', 'score': 10}, 
-            {'text': 'red', 'score': 5}, 
-            {'text': 'yellow', 'score': 6},
-          ],
-        },
-        {
-          'questionsText': 'What is your favorite animal', 
-          'answers':[
-            {'text': 'cat', 'score': 10},
-            {'text': 'dog', 'score': 6},
-            {'text': 'sheep', 'score': 5},
-          ],
-        },
-        {
-          'questionsText': 'What is your favorite food', 
-          'answers':[
-            {'text': 'bread', 'score': 10},
-            {'text': 'rice', 'score': 6},
-            {'text': 'beans', 'score': 5},
-          ],
-        },
-      ];
-
-  var _questionIndex = 0;
-  var _totalScore    = 0;
-
-  void _resetQuiz(){
-    setState(() {
-      _questionIndex = 0;
-      _totalScore    = 0;
-    });
-  }
-  void _answerQuestion(int score) {
-    _totalScore += score;
-    if (_questionIndex < _questions.length) {
-        setState(() {
-          _questionIndex += 1;
-        });
-    } else {
-        print('Question finished!');
-    }
-    
-    print('This is the answer');
-  }
-
+class MyApp extends StatelessWidget{
+  
   @override
   Widget build(BuildContext context){
     
@@ -72,13 +13,9 @@ class _MyAppState extends State<MyApp> {
         title: Text('Quiz App'),
         backgroundColor: Color.fromARGB(255, 14, 13, 72),
       ),
-        body: _questionIndex < _questions.length ?  
-          Quiz(
-            answerQuestion: _answerQuestion, 
-            questionIndex: _questionIndex, 
-            questions: _questions,
-          )
-         : Result(_totalScore, _resetQuiz),
+        body: Center(
+          child: Text('Widget Playing Background'),
+        )
       ), 
     ); 
   }
