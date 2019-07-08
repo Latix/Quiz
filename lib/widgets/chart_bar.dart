083ac84fbdcaefddb7c231b77deb5775c4af020a
Kamsi Kodi <kamsikodi@gmail.com>
2019-07-08 09:12:41 +0100

    Modified Chart UI

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -13,7 +13,7 @@ class ChartBar extends StatelessWidget {
         Container(
           height: 20,
           child: FittedBox(
-            child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+            child: Text('\$${spendingAmount.toStringAsFixed(0)}', style: TextStyle(fontSize: 100,),),
           ),
         ),
         SizedBox(height: 4,),
@@ -21,17 +21,27 @@ class ChartBar extends StatelessWidget {
           height: 60,
           width: 40,
           child: Stack(children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey,width: 1.0),
-                color: Color.fromRGBO(220, 220, 220, 1),
-                borderRadius: BorderRadius.circular(20),
+              Center(
+                child: Container(
+                  width: 10,
+                  decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey,width: 1.0),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(20),
+                  ),
                 ),
               ),
               FractionallySizedBox(
-                heightFactor: spendingPctOfTotal, 
-                child: Container(
-                  decoration: BoxDecoration(color: Theme.of(context).primaryColor),),)
+                heightFactor: spendingPctOfTotal,
+                child: Center(
+                  child: Container(
+                    width: 10,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Theme.of(context).primaryColor),
+                    ),
+                ),
+                ),
             ],
           ),
         ),
